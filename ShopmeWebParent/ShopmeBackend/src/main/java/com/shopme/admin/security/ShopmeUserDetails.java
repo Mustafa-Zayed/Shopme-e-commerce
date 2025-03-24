14a88e926b7ebc80eb1c05c3d0489568a8c9f12f
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2025-03-24 17:56:20 +0200

    Code user login page with authentication

---
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -0,0 +1,49 @@
+package com.shopme.admin.security;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class ShopmeUserDetails implements UserDetails {
+
+    private final User user;
+
+    public ShopmeUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        Set<Role> roles = user.getRoles();
+
+        for (Role role : roles) {
+            SimpleGrantedAuthority tempAuthority = new SimpleGrantedAuthority(role.getName());
+            authorities.add(tempAuthority);
+        }
+
+        return authorities;
+    }
+}
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -1,20 +1,45 @@
 package com.shopme.admin.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
+
+    private final ShopmeUserDetailsService shopmeUserDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(shopmeUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                (requests) -> requests
-                        .anyRequest().permitAll()
-        );
+        http
+                .authorizeHttpRequests(
+                        (requests) -> requests
+                                .requestMatchers("/*.css", "/images/**", "/js/**", "/webjars/**", "/fontawesome/**", "/webfonts/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .formLogin(
+                        (form) -> form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/authenticateUser")
+                                .usernameParameter("email")
+                                .permitAll()
+                );
 
         return http.build();
     }
