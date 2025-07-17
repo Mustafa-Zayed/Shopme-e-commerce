8e81085a0cfbee818f3b14c8975ecdea83e50b6b
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2025-07-17 22:04:01 +0300

    Code add details (specs) on product form

---
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/product/product_form_details.js
@@ -0,0 +1,56 @@
+const addMoreDetailBtn = $("#addMoreDetail");
+const allDetailsDivs = $("#productDetailsDiv")
+
+
+let detailCount = 1;
+
+$(document).ready(function () {
+    addMoreDetailBtn.on("click", addMoreDetail);
+});
+
+function addMoreDetail() {
+    // approach 2
+    // const allDetailsRows = $("[id ^= 'detailRow']"); // must be inside the function as it's not fixed and will increase dynamically
+    // const allDetailsRowsCounts = allDetailsRows.length;
+    // const nextDivDetailId = allDetailsRowsCounts // we can replace detailCount with nextDivDetailId
+
+    let htmlDetailSection = `
+        <div class="row mb-3" id="detailRow${detailCount}">
+            <div class="col-4 d-flex align-items-center">
+                <label for="name${detailCount}" class="me-2 mb-0">Name:</label>
+                <input type="text" class="form-control" id="name${detailCount}" name="detailName" maxlength="255">
+            </div>
+        
+            <div class="col-4 d-flex align-items-center">
+                <label for="value${detailCount}" class="me-2 mb-0">Value:</label>
+                <input type="text" class="form-control" id="value${detailCount}" name="detailValue" maxlength="255">
+            </div>
+        </div>
+    `
+    // approach 2
+    // let prevDetailDiv = allDetailsRows.last();
+    // let prevDetailDivID = prevDetailDiv.attr("id");
+
+    let htmlLinkRemove = `
+        <div class="col-2">
+            <a class="btn fa-solid fa-times-circle fa-2x icon-dark" 
+            href="javascript:removeDetailSection(${detailCount - 1})"
+            title="Remove this detail"
+            ></a>
+        </div>
+    `
+    // href="javascript:removeDetailSection('${prevDetailDivID}')" // approach 2
+
+    allDetailsDivs.append(htmlDetailSection);
+    $("#detailRow" + (detailCount - 1)).append(htmlLinkRemove);
+    // prevDetailDiv.append(htmlLinkRemove); // approach 2
+
+    $("input[name='detailName']").last().focus(); // move the focus to the last row
+
+    detailCount++;
+}
+
+function removeDetailSection(index) {
+    $("#detailRow" + index).remove();
+    // $("#" + index).remove(); // approach 2
+}
--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/product/product_form_images.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/product/product_form_images.js
@@ -1,18 +1,8 @@
-const brandSelect = $("#brand");
-const categorySelect = $("#category");
 const productForm = $("#productForm");
 
 let extraImageCount = 1;
 
 $(document).ready(function () {
-    // Initial call (for default brand selected when the page loads)
-    getCategories();
-
-    // Update categories on change
-    brandSelect.on("change", getCategories);
-
-    $('.content').richText();
-
     $('input[name="extraImage"]').each(function (index){
         $(this).change(function () {
             checkSizeAndShowThumbnailAndAddNewExtraImage(this, $("#extraThumbnail0"), index);
@@ -135,50 +125,4 @@ function removeLastExtraImage(){
         if (this.value === "")
             this.remove();
     })
-}
-
-function getCategories() {
-    let brandId = brandSelect.val(); // brandSelect.find("option:selected").val();
-    let url = brandModuleURL + "/" + brandId + "/categories";
-
-    $.get(
-        url,
-        function (response) {
-            categorySelect.empty();
-            $.each(response, function (index, category) {
-                $("<option>")
-                    .val(category.id)
-                    .text(category.name)
-                    .appendTo(categorySelect);
-                // categorySelect.append($("<option></option>")
-                //     .val(category.id)
-                //     .text(category.name));
-            });
-        });
-}
-
-function checkUniqueName(form) {
-    let productName = $("#name").val();
-    let productId = $("#id").val();
-    let csrfValue = $("input[name='_csrf']").val(); // the server generates a unique CSRF token and sends it to the client (e.g., embedded in the HTML or as part of a response header).
-    let params = {name: productName, id: productId, _csrf: csrfValue};
-
-    $.post(
-        productUniqueNameUrl,
-        params,
-        function (response) {
-            if (response === "OK")
-                // alert("OK")
-                form.submit();
-            else if (response === "Duplicated")
-                showWarningModal(`There is another product having the name: <b>${productName}</b>`);
-            else
-                showErrorModal("Unknown response from the server.");
-        }
-    ).fail(function () { // in case it fails to connect to the server (i.e '/products/check_unique_name' API)
-            showErrorModal("Could not connect to server.")
-        }
-    )
-
-    return false;
 }
